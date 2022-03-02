40442e5406eb3b4eace079b674d77e42fca44802
anunezmartinez <ahriannm00@gmail.com>
2022-03-02 12:08:37 +0100

    commit

--- Tema 4/Ejercicio 3/Main.c
+++ Tema 4/Ejercicio 3/Main.c
@@ -1,29 +1,53 @@
 #include <stdio.h>
 #include <math.h>
 
-int pide_lados ();
-void pide_radio (float *);
-void perímetro (int, float);
-void longitud_lado (float);
-void area (float, float);
-float apotema (float);
+int pide_lados();
+void pide_radio(float *);
+void perimetro(int,float);
+void longitud_lado(int, float);
+void area(float, float);
+float apotema(float);
 
-int main(){
+int main()
+{
+    float radio = 0;
+    pide_radio(&radio);
+    int lados = 0;
+    lados = pide_lados();
 
+    
+    longitud_lado(lados, radio);
 
-
+    parimetro( ,lados);
 
     return 0;
 }
 
-int pide_lados(){
-
-return  ;
-
+int pide_lados()
+{
+    int nlads = 0;
+    nlads = getc(stdin);
+    return nlads;
 }
 
-void pide_radio(float *num1){
+void pide_radio(float *num1)
+{
 
     scanf("%f", num1);
+}
+
+void logintud_lado(int lados, float radio)
+{
+
+    int lado = sqrt(2 * exp(radio) * (1 - cos(360 / lados)));
+
+    float perimetro = lado * lados;
+    printf("El perímetro es %f\n",perimetro);
+}
+
+void perimetro(int num1, float num2)
+{
+    
+
 
 }
